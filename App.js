9f87fdaee192b5d9b96081859d0a7b753875e72d
ppoliani <ppoliani@gmail.com>
2017-10-20 17:56:24 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import Pie from './js/Pie';
+import Theme from './js/theme';
+import data from './resources/data';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      spendingsPerYear: data.spendingsPerYear,
+    };
+  }
+
+
+  shuffle(a) {
+      for (let i = a.length; i; i--) {
+          let j = Math.floor(Math.random() * i);
+          [a[i - 1], a[j]] = [a[j], a[i - 1]];
+      }
+      return a;
+  }
+
+  renderContent(selectedValue) {
+    return <Text style={styles.text}>{selectedValue.name}</Text>
+  }
+
+  render() {
+    const height = 500;
+    const width = 500;
+
+    return (
+      <ScrollView>
+        <View style={styles.container} >
+          <Text style={styles.chart_title}>Distribution of spending this month</Text>
+          <Pie
+            pieWidth={300}
+            pieHeight={300}
+            onItemSelected={() => {}}
+            colors={Theme.colors}
+            width={width}
+            height={height}
+            renderContent={this.renderContent}
+            data={data.spendingsLastMonth} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    backgroundColor:'whitesmoke',
+    marginTop: 21,
+  },
+  chart_title : {
+    paddingTop: 15,
+    textAlign: 'center',
+    paddingBottom: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    backgroundColor:'white',
+    color: 'grey',
+    fontWeight:'bold',
+  },
+  text: {
+    fontSize: 20,
+    backgroundColor: 'transparent'
+  }
+}
